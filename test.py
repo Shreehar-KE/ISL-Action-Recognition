db079c35d87d2a165f6df4208610b5eb3ed15b55
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-09-18 16:00:27 +0530

    update test.py

--- test.py
+++ test.py
@@ -5,6 +5,20 @@ mp_holistic = utils.mp_holistic
 mp_drawing = utils.mp_drawing
 
 
+def mediapipe_detection(image, model):
+    """To process each frame/image & detect using MediaPipe model"""
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    return image, results
+
+
 cap = cv2.VideoCapture(0)
 # Set mediapipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -14,7 +28,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         ret, frame = cap.read()
 
         # Make detections
-        image, results = utils.mediapipe_detection(frame, holistic)
+        image, results = mediapipe_detection(frame, holistic)
         print(results)
 
         # Draw landmarks
